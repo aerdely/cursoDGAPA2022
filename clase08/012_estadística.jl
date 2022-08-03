0b845874a0bc4bad255d429d0499114a94d70003
Arturo Erdely <arturoerdely@gmail.com>
2022-08-03 11:08:42 -0500

    Update 012_estadística.jl

--- clase08/012_estadística.jl
+++ clase08/012_estadística.jl
@@ -4,6 +4,8 @@ using Distributions, Plots, LaTeXStrings
 
 include("EDA.jl")
 
+@doc EDA
+
 
 # Ejemplo 1: mín f(x) = (x - 5)⁴ - 16(x - 5)² + 5(x - 5) + 120
 
@@ -19,7 +21,20 @@ begin # gráfica
 end
 
 
-# Ejemplo 2: mín f(x,y) = |x - 5| + |y + 2|
+## Ejemplo 2: mín g(x) = -x*(x ≤ 0) + (x + 1)*(x > 0)
+
+begin # gráfica
+    x = collect(range(-1, 1, length = 1_000))
+    scatter(x, g.(x), markersize = 1.0, legend = false, xlabel = L"x", ylabel = L"g(x)")
+end
+
+begin
+    g(x) = -x[1]*(x[1] ≤ 0) + (x[1] + 1)*(x[1] > 0)
+    sol = EDA(g, [-1], [1])
+end
+
+
+# Ejemplo 3: mín f(x,y) = |x - 5| + |y + 2|
 
 begin
     f(z) = abs(z[1] - 5) + abs(z[2] + 2)
@@ -29,7 +44,7 @@ end
 sol = EDA(f, [-10, -10], [10, 10], iEnteros = [1, 2]) # solo valores enteros
 
 
-# Ejemplo 3: Estimación por máxima verosimilitud
+# Ejemplo 4: Estimación por máxima verosimilitud
 
 begin
     X = Normal(-2, 5)
